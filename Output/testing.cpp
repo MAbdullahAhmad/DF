3d81192e1b3baa8e4f6aca1cc3e643bcf952e112
Ahmed Raza Attari <turkahmedattari@gmail.com>
2022-06-13 12:34:58 -0400

    Message for Mr.Anderson...

--- Output/testing.cpp
+++ Output/testing.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main()
 {
-    cout<< "Hello check mic 123 testing......";
+    cout<< "Hello check mic 123 testing......\n";
+    cout<< "Hello Mr. Anderson, Attari here! \nnow should we start our work here so that we can easily complete it with in the deadline.....";
 
     return 0;
 }
